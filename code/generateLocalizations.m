d7e2ee8cbecc9e39bce1344d5e6fc863e96c5800
weihong <35338985+whyeo@users.noreply.github.com>
2022-10-11 19:55:29 -0500

    Update generateLocalizations.m
    updated localization uncertainty distribution

--- code/generateLocalizations.m
+++ code/generateLocalizations.m
@@ -2,7 +2,8 @@ function localizations = generateLocalizations(nup_param,loc_uncertainty)
 % This function generates localizations based on the expected number of
 % blinks given by nup_params.
 % 
-% We assume a normally distributed localization uncertainty of 15 nm by default.
+% We assume a lognormally distributed localization uncertainty of 2.29608,0.401825 nm by default.
+%
 %
 % Created by Weihong Yeo, Northwestern University, 2022-08-19.
 %
@@ -15,7 +16,7 @@ else
     n_blinks = round(nbinrnd(r,exp(-0.5)));
 end
 if nargin < 2
-    loc_uncertainty = 15+2*randn(2,n_blinks);
+    loc_uncertainty = repmat(random('lognormal',2.29608,0.401825,1,n_blinks),2,1);
 end
 
 localizations = [randn(2,n_blinks).*loc_uncertainty; zeros(1,n_blinks)];
